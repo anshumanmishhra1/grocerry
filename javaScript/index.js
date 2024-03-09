9cc307f66dd8518f032352e4d3ea1e46e4746b6a
anshumanmishhra1 <anshumanmishhra1@gmail.com>
2024-03-09 23:23:32 +0530

    Loader

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,5 +1,31 @@
 const profileIcon = document.querySelector(".profile");
+const HomeIcon = document.querySelector(".home");
+const loginButton = document.querySelector(".login-btn");
+const SignuP = document.querySelector(".create-account");
+const cartButton = document.querySelector(".product-button");
+const cartQuantity = document.querySelector(".cart-quantity");
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load",()=>{
+    loader.style.display= "none";
+})
 
 profileIcon.addEventListener("click",()=>{
-    window.location.href="login.html"
+    window.location.href="login.html"    
+});
+
+HomeIcon.addEventListener("click",()=>{
+    window.location.href="index.html"
 })
+
+SignuP.addEventListener("click",()=>{
+    window.location.href="profile.html"
+})
+
+let qty = 0;
+
+cartButton.addEventListener("click",()=>{
+    qty++;
+    cartQuantity.innerText=qty;
+});
+
